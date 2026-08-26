2f5e8c56dd078f4f9e3186a84ab0160f0a5c654b
Glas <48269777+DJTOMATO@users.noreply.github.com>
2026-08-26 07:24:36 -0400

    Change max range value from 4 to 2.5
    Per developer request

--- SpeakiMod.js
+++ SpeakiMod.js
@@ -578,7 +578,7 @@ document.body.appendChild(
 				buildElement("input", {
 					type: "range",
 					min: "0.1",
-					max: "4",
+					max: "2.5",
 					step: "0.1",
 					value: window.BeyBladeSpeed || 1,
 					style: "width: 110px; height: 3px; accent-color: #fff; cursor: pointer; margin: 0;",
